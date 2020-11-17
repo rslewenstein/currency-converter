cd024d75b0e051566eba33983d0fe82ebe00d184
rslewenstein <rafasoaresjf@gmail.com>
2020-11-17 00:26:46 -0300

    Add more currency

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="app">
-    <Converter cur-a="USD" cur-b="BRL" />
+    <h1>Currency Converter</h1>
+    <div class="line">
+      <Converter cur-a="USD" cur-b="BRL" />
+      <Converter cur-a="BRL" cur-b="USD" />
+    </div>
+    
+    <div class="line">
+      <Converter cur-a="EUR" cur-b="BRL" />
+      <Converter cur-a="BRL" cur-b="EUR" />
+    </div>
   </div>
 </template>
 
@@ -18,11 +27,18 @@ export default {
 
 <style>
 #app {
+  margin: auto;
+  max-width: 700px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.line{
+  margin-top: 40px;
+  display: flex;
+  justify-content: space-around;
 }
 </style>
